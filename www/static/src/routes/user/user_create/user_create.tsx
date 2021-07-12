09c5ced15751a0272e452d132d2930f770145f74
hahacoo <zhangdi525@163.com>
2021-07-12 18:44:45 +0800

    feat: 删除企业图片展示

--- www/static/src/routes/user/user_create/user_create.tsx
+++ www/static/src/routes/user/user_create/user_create.tsx
@@ -15,7 +15,6 @@ interface UserCreateProps extends FormComponentProps, RouteComponentProps<any> {
 @inject('userStore')
 @observer
 class UserCreateForm extends React.Component<UserCreateProps, any> {
-
     private id: string;
     public userStore: any;
 
@@ -32,7 +31,6 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
         } else {
             this.setUserInfoEmpty();
         }
-
     }
 
     componentWillReceiveProps(nextProps: any) {
@@ -53,7 +51,7 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
             type: '',
             email: '',
             app_key: '',
-            app_secret: ''
+            app_secret: '',
         });
     }
 
@@ -69,7 +67,9 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
                 if (this.id && values.password === '') {
                     delete values.password;
                 } else {
-                    let password = md5(window.SysConfig.options.password_salt + values.password);
+                    let password = md5(
+                        window.SysConfig.options.password_salt + values.password,
+                    );
                     values.password = password;
                 }
                 this.userStore.setSubmitting(true);
@@ -77,17 +77,20 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
                     values.id = this.id;
                 }
 
-                this.userStore.saveUser(values, () => {
-                    message.success(this.id ? '保存成功' : '添加成功');
-                    this.userStore.setSubmitting(false);
-                    setTimeout(() => this.props.history.push('/user/list'), 1000);
-                },                      () => {
-                    message.error('保存失败');
-                    this.userStore.setSubmitting(false);
-                });
+                this.userStore.saveUser(
+                    values,
+                    () => {
+                        message.success(this.id ? '保存成功' : '添加成功');
+                        this.userStore.setSubmitting(false);
+                        setTimeout(() => this.props.history.push('/user/list'), 1000);
+                    },
+                    () => {
+                        message.error('保存失败');
+                        this.userStore.setSubmitting(false);
+                    },
+                );
             }
         });
-
     }
 
     // 生成密钥
@@ -104,7 +107,7 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
      */
     isDisabled(type: string) {
         let prop = {
-            disabled: false
+            disabled: false,
             // value: this.userStore.userInfo[type] || '',
             // onChange: this.changeInput.bind(this, type)
         };
@@ -120,9 +123,16 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
 
         let options = window.SysConfig.options;
         let ldapOn = options.ldap_on === '1' ? true : false;
-        let ldapWhiteList = options.ldap_whiteList ? options.ldap_whiteList.split(',') : [];
+        let ldapWhiteList = options.ldap_whiteList
+            ? options.ldap_whiteList.split(',')
+            : [];
         let editUserName = this.userStore.userInfo.name || '';
-        if (this.id && ldapOn && ['type', 'status'].indexOf(type) === -1 && ldapWhiteList.indexOf(editUserName) === -1) {
+        if (
+            this.id &&
+            ldapOn &&
+            ['type', 'status'].indexOf(type) === -1 &&
+            ldapWhiteList.indexOf(editUserName) === -1
+        ) {
             prop.disabled = true;
         }
 
@@ -136,7 +146,7 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
         } else {
             callback();
         }
-    }
+    };
 
     /**
      * render
@@ -162,11 +172,19 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
                 <div className="fk-content-wrap">
                     <BreadCrumb {...this.props} />
                     <div className="manage-container">
-                        <h3 style={{marginBottom: '20px'}}>LDAP提示</h3>
-                        <p>本系统已开启LDAP认证服务，LDAP服务开启后，本系统不能新增用户，且用户的用户名、密码、邮箱、别名均由LDAP统一管理，本系统不能修改。</p>
+                        <h3 style={{ marginBottom: '20px' }}>LDAP提示</h3>
+                        <p>
+                            本系统已开启LDAP认证服务，LDAP服务开启后，本系统不能新增用户，且用户的用户名、密码、邮箱、别名均由LDAP统一管理，本系统不能修改。
+                        </p>
                         <div className="alert alert-warning" role="alert">
                             如需要修改，请使用给本系统提供LDAP服务的
-                            {ldap_user_page ? <a href={ldap_user_page} target="_blank">用户管理服务</a> : '用户管理服务'}
+                            {ldap_user_page ? (
+                                <a href={ldap_user_page} target="_blank">
+                                    用户管理服务
+                                </a>
+                            ) : (
+                                '用户管理服务'
+                            )}
                             ，或者联系系统管理员。
                         </div>
                     </div>
@@ -185,52 +203,94 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
                         <div className="pull-left">
                             <FormItem label="用户名">
                                 {getFieldDecorator('username', {
-                                    rules: [{
+                                    rules: [
+                                        {
                                             min: 4,
                                             max: 20,
-                                            message: '长度为4到20个字符'
+                                            message: '长度为4到20个字符',
                                         },
                                         {
                                             required: true,
-                                            message: '请输入用户名!'
-                                        }],
+                                            message: '请输入用户名!',
+                                        },
+                                    ],
                                     initialValue: userInfo.name ? userInfo.name : '',
-                                })(<Input {...this.isDisabled('name')} placeholder="4-20个字符"/>)}
-                                <p className="help-block">登录时所用的名称，不能重复。</p>
+                                })(
+                                    <Input
+                                        {...this.isDisabled('name')}
+                                        placeholder="4-20个字符"
+                                    />,
+                                )}
+                                <p className="help-block">
+                                    登录时所用的名称，不能重复。
+                                </p>
                             </FormItem>
                             <FormItem label="邮箱">
                                 {getFieldDecorator('email', {
-                                    rules: [{
+                                    rules: [
+                                        {
                                             required: true,
-                                            message: '请输入邮箱!'
-                                        }, {
-                                        type: 'email',
-                                        message: '邮箱格式错误!'
-                                    }],
+                                            message: '请输入邮箱!',
+                                        },
+                                        {
+                                            type: 'email',
+                                            message: '邮箱格式错误!',
+                                        },
+                                    ],
                                     initialValue: userInfo.email ? userInfo.email : '',
-                                })(<Input {...this.isDisabled('email')} autoComplete="email" placeholder="输入邮箱"/>)}
-                                <p className="help-block">用户主要联系方式，不能重复。</p>
+                                })(
+                                    <Input
+                                        {...this.isDisabled('email')}
+                                        autoComplete="email"
+                                        placeholder="输入邮箱"
+                                    />,
+                                )}
+                                <p className="help-block">
+                                    用户主要联系方式，不能重复。
+                                </p>
                             </FormItem>
                             <FormItem label="密码">
                                 {getFieldDecorator('password', {
-                                    rules: [{
-                                        required: true, message: '请输入密码!',
-                                    }, {
-                                        min: 8,
-                                        max: 30,
-                                        message: '长度为8到30个字符'
-                                    }],
-                                })(<Input {...this.isDisabled('password')} type="password" placeholder="长度为8到30个字符" />)}
-                                <p className="help-block">建议使用特殊字符与字母、数字的混编方式，增加安全性。</p>
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请输入密码!',
+                                        },
+                                        {
+                                            min: 8,
+                                            max: 30,
+                                            message: '长度为8到30个字符',
+                                        },
+                                    ],
+                                })(
+                                    <Input
+                                        {...this.isDisabled('password')}
+                                        type="password"
+                                        placeholder="长度为8到30个字符"
+                                    />,
+                                )}
+                                <p className="help-block">
+                                    建议使用特殊字符与字母、数字的混编方式，增加安全性。
+                                </p>
                             </FormItem>
                             <FormItem label="确认密码">
                                 {getFieldDecorator('repassword', {
-                                    rules: [{
-                                        required: true, message: '请再次确认密码!',
-                                    }, {
-                                        validator: this.compareToFirstPassword,
-                                    }]
-                                })(<Input {...this.isDisabled('repassword')} type="password" placeholder="请再次输入密码" />)}
+                                    rules: [
+                                        {
+                                            required: true,
+                                            message: '请再次确认密码!',
+                                        },
+                                        {
+                                            validator: this.compareToFirstPassword,
+                                        },
+                                    ],
+                                })(
+                                    <Input
+                                        {...this.isDisabled('repassword')}
+                                        type="password"
+                                        placeholder="请再次输入密码"
+                                    />,
+                                )}
                             </FormItem>
                             <button type="submit" {...props} className="btn btn-primary">
                                 {this.userStore.submitting ? '提交中...' : '提交'}
@@ -239,48 +299,83 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
                         <div className="pull-left">
                             <FormItem label="别名">
                                 {getFieldDecorator('display_name', {
-                                    initialValue: userInfo.display_name ? userInfo.display_name : '',
-                                })(<Input {...this.isDisabled('display_name')} placeholder="显示名称" />)}
+                                    initialValue: userInfo.display_name
+                                        ? userInfo.display_name
+                                        : '',
+                                })(
+                                    <Input
+                                        {...this.isDisabled('display_name')}
+                                        placeholder="显示名称"
+                                    />,
+                                )}
                             </FormItem>
                             <FormItem label="用户组">
                                 {getFieldDecorator('type', {
-                                    initialValue:  userInfo.type ? userInfo.type.toString() : '',
-                                })(<Select {...this.isDisabled('type')} className="form-control">
-                                    <Option value="2">编辑</Option>
-                                    <Option value="1">管理员</Option>
-                                    <Option value="3">投稿者</Option>
-                                </Select>)}
+                                    initialValue: userInfo.type
+                                        ? userInfo.type.toString()
+                                        : '',
+                                })(
+                                    <Select
+                                        {...this.isDisabled('type')}
+                                        className="form-control"
+                                    >
+                                        <Option value="2">编辑</Option>
+                                        <Option value="1">管理员</Option>
+                                        {/* <Option value="3">投稿者</Option> */}
+                                    </Select>,
+                                )}
                             </FormItem>
                             <FormItem label="状态">
                                 {getFieldDecorator('status', {
-                                    initialValue: userInfo.status ? userInfo.status.toString() : '',
-                                })(<Select {...this.isDisabled('status')} className="form-control">
+                                    initialValue: userInfo.status
+                                        ? userInfo.status.toString()
+                                        : '',
+                                })(
+                                    <Select
+                                        {...this.isDisabled('status')}
+                                        className="form-control"
+                                    >
                                         <Option value="1">有效</Option>
                                         <Option value="2">禁用</Option>
-                                    </Select>)}
+                                    </Select>,
+                                )}
                             </FormItem>
                             {/*<filedset>*/}
-                                <legend>
-                                    认证
-                                    <button
-                                        type="button"
-                                        className="btn btn-primary"
-                                        style={{marginLeft: 15, marginBottom: 5, padding: '3px 5px'}}
-                                        onClick={() => this.generateKey()}
-                                    >重新生成</button>
-                                </legend>
-                                <div className="form-group">
-                                    <label>App Key</label>
-                                    <div>
-                                        <Input type="text" className="form-control" disabled={true}
-                                               value={this.userStore.userInfo.app_key} />
-                                    </div>
+                            {/* <legend>
+                                认证
+                                <button
+                                    type="button"
+                                    className="btn btn-primary"
+                                    style={{
+                                        marginLeft: 15,
+                                        marginBottom: 5,
+                                        padding: '3px 5px',
+                                    }}
+                                    onClick={() => this.generateKey()}
+                                >
+                                    重新生成
+                                </button>
+                            </legend>
+                            <div className="form-group">
+                                <label>App Key</label>
+                                <div>
+                                    <Input
+                                        type="text"
+                                        className="form-control"
+                                        disabled={true}
+                                        value={this.userStore.userInfo.app_key}
+                                    />
                                 </div>
-                                <div className="form-group">
-                                    <label>App Secret</label>
-                                    <Input type="text" className="form-control" disabled={true}
-                                           value={this.userStore.userInfo.app_secret} />
-                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>App Secret</label>
+                                <Input
+                                    type="text"
+                                    className="form-control"
+                                    disabled={true}
+                                    value={this.userStore.userInfo.app_secret}
+                                />
+                            </div> */}
                             {/*</filedset>*/}
                         </div>
                     </Form>
@@ -288,7 +383,6 @@ class UserCreateForm extends React.Component<UserCreateProps, any> {
             </div>
         );
     }
-
 }
 const UserCreate = Form.create()(UserCreateForm);
 export default UserCreate;
